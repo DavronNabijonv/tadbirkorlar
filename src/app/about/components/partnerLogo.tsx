44efa175b0a7c4b737f751a50698e10e2dd43e7c
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-07-15 14:02:56 +0500

    partners logos

--- src/app/about/components/partnerLogo.tsx
+++ src/app/about/components/partnerLogo.tsx
@@ -9,7 +9,7 @@ export default function PartnerLogo() {
         desc="“Business Diplomatia” platformasi dunyo bo‘ylab faoliyat yuritayotgan ishonchli tashkilotlar, universitetlar, kompaniyalar va inkubatsion markazlar bilan hamkorlikda ishlaydi."
       />
 
-        <img src={logos} className="mx-auto mt-10" />
+        <img src={logos} className="mx-auto mt-10 w-auto h-auto" />
 
     </div>
   );
