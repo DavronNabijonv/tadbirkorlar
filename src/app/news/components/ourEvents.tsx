a16e51fb84c7eca71e2752ca85eb629396d86a90
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-07-18 16:33:23 +0500

    nims gap

--- src/app/news/components/ourEvents.tsx
+++ src/app/news/components/ourEvents.tsx
@@ -2,7 +2,7 @@ import { w1, w2, w3, w4, w5, w6, w7, w8, w9 } from "@/assets";
 import Title from "@/components/shared/Title";
 import { useRef, useState } from "react";
 import type { VideoNewsItem } from "@/types"; // optional umuman optio
-import VideoCard from "@/components/shared/videoCard";
+import VideoCard from "@/components/shared/VideoCard";
 
 const videoNews: VideoNewsItem[] = [
   { videos: w1, title: "Hamkorlarimiz", desc: "“Business Diplomatia” platformasi...", links: "#" },
