2a23cd1da99d640f098dfe8a16d9bd8ca9223c0c
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-07-18 16:20:44 +0500

    salom

--- src/app/events/components/eventShow.tsx
+++ src/app/events/components/eventShow.tsx
@@ -1,6 +1,8 @@
 import { eventShow } from "@/assets";
 import { PiShoppingBagOpenBold } from "react-icons/pi";
 import ShowCase from "@/components/shared/showCase";
+import { IoStorefrontSharp } from "react-icons/io5";
+import { MdMenuBook } from "react-icons/md";
 
 // FadeInSection.tsx
 import { motion } from "framer-motion";
@@ -42,40 +44,46 @@ export default function EventShow() {
             Kelajak sari birgalikda: Biznes diplomatiyasi maydonida
           </p>
         </motion.div>
-        <motion.div ref={ref}
+        <motion.div
+          ref={ref}
           initial={{ opacity: 0, y: 30 }}
           animate={inView ? { opacity: 1, y: 0 } : {}}
           transition={{
             duration: 0.5,
             delay: 2 * 0.2,
             ease: "easeOut",
-          }} className="group flex w-full max-w-[300px] flex-col items-start justify-start gap-3 rounded-[8px] bg-white p-5 shadow-xl/30 delay-100 duration-100 hover:bg-[#0062AD]">
+          }}
+          className="group flex w-full max-w-[300px] flex-col items-start justify-start gap-3 rounded-[8px] bg-white p-5 shadow-xl/30 delay-100 duration-100 hover:bg-[#0062AD]"
+        >
           <span className="text-[50px] text-[#0062AD] group-hover:text-white">
-            <PiShoppingBagOpenBold />
+          <IoStorefrontSharp />
           </span>
           <p className="text-[24px] font-[500] text-[#1E242C] group-hover:text-white">
-            Diplomatik Aloqalar
+            Biznes Uchrashuvlar
           </p>
           <p className="text-[16px] font-[400] text-[#414D60] group-hover:text-white">
-            Kelajak sari birgalikda: Biznes diplomatiyasi maydonida
+            Hamkorlik ruhi: Professional aloqalarning kuchi
           </p>
         </motion.div>
-        <motion.div ref={ref}
+        <motion.div
+          ref={ref}
           initial={{ opacity: 0, y: 30 }}
           animate={inView ? { opacity: 1, y: 0 } : {}}
           transition={{
             duration: 0.5,
             delay: 3 * 0.2,
             ease: "easeOut",
-          }} className="group flex w-full max-w-[300px] flex-col items-start justify-start gap-3 rounded-[8px] bg-white p-5 shadow-xl/30 delay-100 duration-100 hover:bg-[#0062AD]">
+          }}
+          className="group flex w-full max-w-[300px] flex-col items-start justify-start gap-3 rounded-[8px] bg-white p-5 shadow-xl/30 delay-100 duration-100 hover:bg-[#0062AD]"
+        >
           <span className="text-[50px] text-[#0062AD] group-hover:text-white">
-            <PiShoppingBagOpenBold />
+          <MdMenuBook />
           </span>
           <p className="text-[24px] font-[500] text-[#1E242C] group-hover:text-white">
-            Diplomatik Aloqalar
+            Aloqalarni kuchaytirish
           </p>
           <p className="text-[16px] font-[400] text-[#414D60] group-hover:text-white">
-            Kelajak sari birgalikda: Biznes diplomatiyasi maydonida
+            Networking vaqtida imkoniyatlar yaraladi
           </p>
         </motion.div>
       </div>
